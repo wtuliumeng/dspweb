12ee598791a090676eb5eff15b07f61c6197138b
wtuliumeng <wtuliumeng@gmail.com>
2020-03-10 18:49:59 +0800

    编写数据集管理模块代码 --by liumeng

--- src/components/login.vue
+++ src/components/login.vue
@@ -9,7 +9,7 @@
                     <el-input v-model="formLogin.loginName" placeholder="账号"></el-input>
                 </el-form-item>
                 <el-form-item>
-                    <el-input v-model="formLogin.password" placeholder="密码" ></el-input>
+                    <el-input v-model="formLogin.password" type="password" placeholder="密码" ></el-input>
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary" @click="login">登陆</el-button>
