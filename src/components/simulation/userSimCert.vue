0668ff014577fc2f0e2ab3313c997dcf5fba6205
wangsx <1085740378@qq.com>
2020-03-11 21:01:00 +0800

    wangsx 按钮事件，隐藏绑定

--- src/components/simulation/userSimCert.vue
+++ src/components/simulation/userSimCert.vue
@@ -1,49 +1,50 @@
-
 <template>
   <div class="container certForm1">
-    <el-form :model="certForm" class="demo-form-inline" :label-position="labelPosition" :label-width="labelWidth":inline="false" :rules="formRules" ref="certForm1">
-      <el-form-item label="用户名" prop="userName">
-        <el-select v-model="certForm.userName" placeholder="请选择用户">
-          <el-option
-            v-for="item in userList"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-           ></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="密码" prop="password">
-        <el-input v-model="certForm.password" placeholder="请输入密码"></el-input>
-      </el-form-item>
-      <el-form-item label="IP" prop="ipAddr">
-        <el-input v-model="certForm.ipAddr" placeholder="请输入IP"></el-input>
-      </el-form-item>
-      <el-form-item label=" ">
+    <div>
+      <el-form :model="certForm" class="demo-form-inline" :label-position="labelPosition" :label-width="labelWidth"
+        :inline="false" :rules="formRules" ref="certForm1">
+        <el-form-item label="用户名" prop="userName">
+          <el-select v-model="certForm.userName" placeholder="请选择用户">
+            <el-option v-for="item in userList" :key="item.value" :label="item.label" :value="item.value"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="certForm.password" placeholder="请输入密码"></el-input>
+        </el-form-item>
+        <el-form-item label="IP" prop="ipAddr">
+          <el-input v-model="certForm.ipAddr" placeholder="请输入IP"></el-input>
+        </el-form-item>
+        <el-form-item label=" ">
           <el-button type="primary" @click="onSubmit">认证</el-button>
-      </el-form-item>
-      <hr />
-      <br />
-      <el-form-item label="认证结果" prop="result">
-        <el-input type="textarea" :height="textHeight" readonly="true" resize="none" v-model="certForm.result" placeholder="认证结果"></el-input>
-      </el-form-item>
-    </el-form>
-
+        </el-form-item>
+        <hr />
+        <br />
+      </el-form>
+    </div>
+    <!--:model="resultForm"-->
+    <div>
+      <el-form :model="certResult" v-show="isShow" class="demo-form-inline" :label-position="labelPosition" :label-width="labelWidth"
+        :inline="false" ref="resultForm1">
+        <el-form-item label="认证结果:"></el-form-item>
+        <el-form-item label="用户名: ">{{certResult.token}}</el-form-item>
+        <el-form-item label="Token令牌:">{{certResult.token}}</el-form-item>
+        <el-form-item label="认证时间: ">{{certResult.authTime}}</el-form-item>
+      </el-form>
+    </div>
   </div>
-
-
 </template>
 
 <script>
-  export default{
+  export default {
     name: "userSimCert",
     data() {
-      return{
+      return {
         labelPosition: "right", //lable对齐方式
         labelWidth: "100px", //lable宽度
         textHeight: "40px",
-        //用户列表
-        userList: [
-          {
+        isShow: false,
+        //用户列表测试数据，待删；
+        userList: [{
             value: "1",
             label: "user1"
           },
@@ -56,17 +57,30 @@
             label: "user3"
           }
         ],
+        //认证结果测试数据，待删；
+        certResult: {
+          userName: "wangsx",
+          token: "123456789987654321",
+          authTime: "2020-03-11",
+          status: "启用"
+        },
         //输入框验证
         formRules: {
-          userName: [
-            { required: true, message: "用户名不能为空", trigger: "blur" }
-          ],
-          password: [
-            { required: true, message: "密码不能为空", trigger: "blur" }
-          ],
-          ipAddr: [
-            { required: true, message: "IP不能为空", trigger: "blur" }
-          ]
+          userName: [{
+            required: true,
+            message: "用户名不能为空",
+            trigger: "blur"
+          }],
+          password: [{
+            required: true,
+            message: "密码不能为空",
+            trigger: "blur"
+          }],
+          ipAddr: [{
+            required: true,
+            message: "IP不能为空",
+            trigger: "blur"
+          }]
         },
         //提交页面数据
         certForm: {
@@ -75,13 +89,33 @@
           ipAddr: "",
           result: ""
         },
+        //返回数据，只展示部分
+        resultForm: {
+          userName: "",
+          password: "",
+          service: "",
+          issuer: "",
+          expireTime: "",
+          authTime: "",
+          token: "",
+          status: "",
+          createTime: "",
+          updateTime: "",
+          IPAddr: "",
+          salt: "",
+          timeout: ""
+        }
       };
+    },
+
+    mounted(){
+      this.getUserNameList();
     },
     methods: {
       onSubmit() {
         this.$refs["certForm1"].validate(valid => {
           if (valid) {
-            this.$alert("表单验证通过，提交成功", "标题名称", {
+            this.$alert("认证成功！", "标题名称", {
               confirmButtonText: "确定",
               callback: action => {
                 //   this.$message({
@@ -90,24 +124,39 @@
                 //   });
               }
             });
+            this.$data.isShow=true;
           } else {
             this.$alert("error!");
             console.log("error submit!!");
             return false;
           }
         });
-      }
+      },
+
+      getUserNameList: function(){
+        // let data={};
+        // this.$ajax({
+        //   url: "/api/sysuser-api/searchUserList",
+        //   type: "get",
+        //   dataType: "json",
+        //   success:function(res){
+        //     res.data.
+        //   }
+        // })
+        console.log("searchUserList");
+      },
     }
   }
 </script>
 
 <style lang="scss">
-.certForm1 {
-  .el-form-item__content {
-    width: 430px;
-    .el-select {
+  .certForm1 {
+    .el-form-item__content {
       width: 430px;
+
+      .el-select {
+        width: 430px;
+      }
     }
   }
-}
 </style>
