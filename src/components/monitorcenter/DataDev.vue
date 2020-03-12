667661bfabed1f05d79e956fa9625af9f5fb1201
周阳 <zhouyang@zhouyangdeMacBook-Pro.local>
2020-03-12 11:33:59 +0800

    同步数据采集服务代码 --by zhouyang

--- src/components/monitorcenter/DataDev.vue
+++ src/components/monitorcenter/DataDev.vue
@@ -14,11 +14,8 @@
                       <el-option  v-for="item in form1.statusOptions"   :key="item.value"  :label="item.label"  :value="item.value" ></el-option>
                    </el-select>
                 </el-form-item>
-<<<<<<< HEAD
-=======
                 <el-form-item></el-form-item>
                 <el-form-item></el-form-item>
->>>>>>> 1a90a8c46c644f0db0102d713df0222d357d2d80
                 <el-button type="primary" @click="onSearch">查询</el-button>
                 <el-button type="warning" plain @click="onReset">重置</el-button>
             </el-form>
